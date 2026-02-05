13aa0175e08a2078f27e19d6873e5dc3291ef543
Arpita Garud <garudarpita200@gmail.com>
2026-02-05 20:58:53 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,9 +1,17 @@
-var firebaseConfig = {
-  apiKey: "PASTE_YOUR_KEY",
-  authDomain: "PROJECT.firebaseapp.com",
-  databaseURL: "https://PROJECT.firebaseio.com",
-  projectId: "PROJECT"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASna8hSZo9B76fnqJ_aGpj8AJcZqMrvUM",
+  authDomain: "olive-26478.firebaseapp.com",
+  projectId: "olive-26478",
+  storageBucket: "olive-26478.firebasestorage.app",
+  messagingSenderId: "712708158433",
+  appId: "1:712708158433:web:3fba3dbc8ac1bf61f058a0"
 };
 
-firebase.initializeApp(firebaseConfig);
-var db = firebase.database();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
